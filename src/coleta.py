e7af359aa95ef0228ac230c17f7488d1ec0d3cde
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-02-02 16:12:19 -0300

    Refactor comments and maintain functionality

--- src/coleta.py
+++ src/coleta.py
@@ -1,12 +1,11 @@
 import json
 
-# Simulação de coleta de dados
+# Simulação de coleta
 novos_posts = [
     {"text": "O país precisa de mais diálogo!", "source":"twitter", "date":"2026-02-03", "geo":"São Paulo"},
     {"text": "Radicais estão exagerando.", "source":"reddit", "date":"2026-02-03", "geo":"Minas Gerais"}
 ]
 
-# Adiciona aos dados existentes
 with open("../data/posts.json", "r") as f:
     posts = json.load(f)
 
